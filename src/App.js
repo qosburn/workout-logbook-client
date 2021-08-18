d603ff6597a676c249fb22d81ede5bbd12d2cada
qosburn <qosburn@mac.com>
2021-08-18 16:52:24 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import Sitebar from './home/Navbar';
+
+function App() {
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, []);
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  };
+  return (
+    <div>
+      <Sitebar />
+    </div>
+  );
+}
+
+export default App;
